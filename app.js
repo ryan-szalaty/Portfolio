512b84927e67e6fe6951671114b3a6bb30f40f5d
ザラテイ・ライアン <szalatyryan@zarateiraians-MacBook-Air.local>
2022-04-24 17:42:50 +0900

    Version 1 complete. Need download file and multiple pages.

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/download', (req, res) => {
+    const file = `${__dirname}/public/files/resume.pdf`;
+    res.download(file);
+});
+
 app.listen(process.env.PORT, () => {
     console.log('Server initiated.'.cyan.underline);
 });
