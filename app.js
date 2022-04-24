e08d0c778767bdef2e18d5183719ce7f8fd3150e
ザラテイ・ライアン <szalatyryan@zarateiraians-MacBook-Air.local>
2022-04-24 16:24:42 +0900

    Refactored page.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 const colors = require('colors');
 
 const app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('HOORAH!');
+    res.render('index');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server initiated.'.cyan.underline);
 });
